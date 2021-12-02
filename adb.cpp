gengjiawen/adb

adb --one-device feature.
Added support for a new server option, --one-device.

When a server is started with that option, it will only communicate with
the USB device serial number or device address specified.

This allows multiple adb servers to run in parallel on a single host,
each communicating only with a single USB device.

example usage:
$ adb --one-device usb:2-4 -P 1138 start-server
$ adb --one-device usb:2-6 -P 1139 start-server
$ adb -P 1138 devices -l
List of devices attached
97221FFBA00016         device usb:2-4 transport_id:1
$ adb -P 1139 devices -l
List of devices attached
13031FDEE0000C         device usb:2-6 transport_id:1

Test: Manually tested as shown above.
Change-Id: Ibc4e3df7ecdefce737291185fa6a84071bff5340

adb.cpp
@@ -726,7 +726,7 @@ static void ReportServerStartupFailure(pid_t pid) {
     while (static_cast<size_t>(i) < lines.size()) fprintf(stderr, "%s\n", lines[i++].c_str());
 }
 
-int launch_server(const std::string& socket_spec) {
+int launch_server(const std::string& socket_spec, const char* one_device) {
 #if defined(_WIN32)
     /* we need to start the server in the background                    */
     /* we create a PIPE that will be used to wait for the server's "OK" */
@@ -828,8 +828,14 @@ int launch_server(const std::string& socket_spec) {
     }
 
     WCHAR   args[64];
-    snwprintf(args, arraysize(args), L"adb -L %s fork-server server --reply-fd %d",
-              socket_spec.c_str(), ack_write_as_int);
+    if (one_device) {
+        snwprintf(args, arraysize(args),
+                  L"adb -L %s fork-server server --reply-fd %d --one-device %s",
+                  socket_spec.c_str(), ack_write_as_int, one_device);
+    } else {
+        snwprintf(args, arraysize(args), L"adb -L %s fork-server server --reply-fd %d",
+                  socket_spec.c_str(), ack_write_as_int);
+    }
 
     PROCESS_INFORMATION   pinfo;
     ZeroMemory(&pinfo, sizeof(pinfo));
@@ -978,8 +984,14 @@ int launch_server(const std::string& socket_spec) {
         char reply_fd[30];
         snprintf(reply_fd, sizeof(reply_fd), "%d", pipe_write.get());
         // child process
-        int result = execl(path.c_str(), "adb", "-L", socket_spec.c_str(), "fork-server", "server",
-                           "--reply-fd", reply_fd, NULL);
+        std::vector<const char*> argv = {
+                "adb", "-L", socket_spec.c_str(), "fork-server", "server", "--reply-fd", reply_fd};
+        if (one_device) {
+            argv.push_back("--one-device");
+            argv.push_back(one_device);
+        }
+        argv.push_back(nullptr);
+        int result = execv(path.c_str(), const_cast<char* const*>(argv.data()));
         // this should not return
         fprintf(stderr, "adb: execl returned %d: %s\n", result, strerror(errno));
     } else {

client/usb_osx.cpp
@@ -299,6 +299,12 @@ AndroidInterfaceAdded(io_iterator_t iterator)
             strncpy(serial, devpath.c_str(), sizeof(serial));
             serial[sizeof(serial) - 1] = 0;
         }
+        if (!transport_server_owns_device(dev_path, serial)) {
+            // We aren't allowed to communicate with this device. Don't open this device.
+            D("ignoring device: not owned by this server dev_path: '%s', serial: '%s'",
+              devpath.c_str(), serial);
+            continue;
+        }
 
         std::unique_ptr<usb_handle> handle =
             CheckInterface((IOUSBInterfaceInterface550**)iface, vendor, product);

LineageOS/android_packages_modules_adb

Test: Manually tested as shown above with & without libusb, Linux & macOS.

adb.cpp
@@ -726,7 +726,7 @@ static void ReportServerStartupFailure(pid_t pid) {
     while (static_cast<size_t>(i) < lines.size()) fprintf(stderr, "%s\n", lines[i++].c_str());
 }
 
-int launch_server(const std::string& socket_spec) {
+int launch_server(const std::string& socket_spec, const char* one_device) {
 #if defined(_WIN32)
     /* we need to start the server in the background                    */
     /* we create a PIPE that will be used to wait for the server's "OK" */
@@ -828,8 +828,14 @@ int launch_server(const std::string& socket_spec) {
     }
 
     WCHAR   args[64];
-    snwprintf(args, arraysize(args), L"adb -L %s fork-server server --reply-fd %d",
-              socket_spec.c_str(), ack_write_as_int);
+    if (one_device) {
+        snwprintf(args, arraysize(args),
+                  L"adb -L %s fork-server server --reply-fd %d --one-device %s",
+                  socket_spec.c_str(), ack_write_as_int, one_device);
+    } else {
+        snwprintf(args, arraysize(args), L"adb -L %s fork-server server --reply-fd %d",
+                  socket_spec.c_str(), ack_write_as_int);
+    }
 
     PROCESS_INFORMATION   pinfo;
     ZeroMemory(&pinfo, sizeof(pinfo));
@@ -978,8 +984,14 @@ int launch_server(const std::string& socket_spec) {
         char reply_fd[30];
         snprintf(reply_fd, sizeof(reply_fd), "%d", pipe_write.get());
         // child process
-        int result = execl(path.c_str(), "adb", "-L", socket_spec.c_str(), "fork-server", "server",
-                           "--reply-fd", reply_fd, NULL);
+        std::vector<const char*> child_argv = {
+                "adb", "-L", socket_spec.c_str(), "fork-server", "server", "--reply-fd", reply_fd};
+        if (one_device) {
+            child_argv.push_back("--one-device");
+            child_argv.push_back(one_device);
+        }
+        child_argv.push_back(nullptr);
+        int result = execv(path.c_str(), const_cast<char* const*>(child_argv.data()));
         // this should not return
         fprintf(stderr, "adb: execl returned %d: %s\n", result, strerror(errno));
     } else {

client/usb_osx.cpp
@@ -296,6 +296,13 @@ AndroidInterfaceAdded(io_iterator_t iterator)
             continue;
         }
 
+        if (!transport_server_owns_device(devpath, serial)) {
+            // We aren't allowed to communicate with this device. Don't open this device.
+            D("ignoring device: not owned by this server dev_path: '%s', serial: '%s'",
+              devpath.c_str(), serial);
+            continue;
+        }
+
         std::unique_ptr<usb_handle> handle =
             CheckInterface((IOUSBInterfaceInterface550**)iface, vendor, product);
         if (handle == nullptr) {