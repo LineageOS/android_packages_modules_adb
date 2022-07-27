5ec6fdabc0e0b7ba6725e02f5207a1c1d5fdbcaf
Fabien Sanglard <sanglardf@google.com>
2022-07-27 15:17:44 -0700

    ADB: Document ConnectionState
    Provide meaning for all "states" in the enum.
    
    Test: NA
    Bug: NA
    Change-Id: Ic2828a893aa74673e2d11db7ee09d4bb681b4e5e

--- adb.h
+++ adb.h
@@ -111,12 +111,14 @@ enum ConnectionState {
     kCsDetached,        // USB device that's detached from the adb server.
     kCsOffline,
 
-    kCsBootloader,
-    kCsDevice,
-    kCsHost,
-    kCsRecovery,
-    kCsSideload,
-    kCsRescue,
+    // After CNXN packet, the ConnectionState describes not a state but the type of service
+    // on the other end of the transport.
+    kCsBootloader,  // Device running fastboot OS (fastboot) or userspace fastboot (fastbootd).
+    kCsDevice,      // Device running Android OS (adbd).
+    kCsHost,        // What a device sees from its end of a Transport (adb host).
+    kCsRecovery,    // Device with bootloader loaded but no ROM OS loaded (adbd).
+    kCsSideload,    // Device running Android OS Sideload mode (minadbd sideload mode).
+    kCsRescue,      // Device running Android OS Rescue mode (minadbd rescue mode).
 };
 
 std::string to_string(ConnectionState state);
