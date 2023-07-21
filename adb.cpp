5e9f9f41a062fc2065a95af7b15f59c42eea243a
Fabien Sanglard <sanglardf@google.com>
2023-07-21 20:21:53 +0000

    Sanitize logging
    Problem: Some parts of adb bypass the tracing system which results in
    more than necessary verbose outputs when running "adb server nodaemon".
    
    Solution: Fix callsite to use the tracing system. The right fix would
    be do not expose android-base/logging.h and make everything go through
    tracing. WARN and ERROR should not go through the tracing mask because
    they are too important but DEBUG and INFO should be discardable.
    
    Test: NA
    Bug: 292700510
    Change-Id: I12c9dea2408a37c3e1dcfc2dab693c881bcb1c83

--- adb.cpp
+++ adb.cpp
@@ -1239,7 +1239,7 @@ HostRequestResult handle_host_request(std::string_view service, TransportType ty
         }
     }
 
-    LOG(DEBUG) << "handle_host_request(" << service << ")";
+    VLOG(SERVICES) << "handle_host_request(" << service << ")";
 
     // Transport selection:
     if (service.starts_with("transport") || service.starts_with("tport:")) {
